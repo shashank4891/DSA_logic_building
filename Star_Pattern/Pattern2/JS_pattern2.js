0327ec8508cc1b8cc1841cac407e472ac65ff6c2
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-07-28 23:31:45 +0530

    Update JS_pattern2.js

--- Star_Pattern/Pattern2/JS_pattern2.js
+++ Star_Pattern/Pattern2/JS_pattern2.js
@@ -1,1 +1,21 @@
+function pattern2(N) {
+    // This is the outer loop which will loop for the rows.
+    for (let i = 0; i < N; i++) {
+        // This is the inner loop which loops for the columns
+        // number of columns = row number for each line here.
+        let row = "";
+        for (let j = 0; j <= i; j++) {
+            row += "* ";
+        }
+        
+        // Print the row and give a line break
+        console.log(row);
+    }
+}
+
+// Take user input using prompt
+const N = parseInt(prompt("Enter the number of rows:"));
+
+// Call the function with the user input
+pattern2(N);
 
