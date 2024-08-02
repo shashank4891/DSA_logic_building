2e65257d892edd01051768b840725552eaee45a2
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-02 22:23:09 +0530

    Update Python_pattern3.py

--- Star_Pattern/Pattern3/Python_pattern3.py
+++ Star_Pattern/Pattern3/Python_pattern3.py
@@ -1,1 +1,10 @@
+def pattern3(N):
+    for i in range(1, N + 1):
+        row = ""
+        for j in range(1, i + 1):
+            row += str(j) + " "
+        print(row)
+
+N = int(input("Enter the number of rows: "))
+pattern3(N)
 
