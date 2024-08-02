8ba6dda77da3b662167cab98b37f0a520fefeb0d
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-02 22:26:03 +0530

    Update Python_pattern5.py

--- Star_Pattern/Pattern5/Python_pattern5.py
+++ Star_Pattern/Pattern5/Python_pattern5.py
@@ -1,1 +1,10 @@
+def pattern5(N):
+    for i in range(N):
+        row = ""
+        for j in range(N, i, -1):
+            row += "* "
+        print(row)
+
+N = int(input("Enter the number of rows: "))
+pattern5(N)
 
