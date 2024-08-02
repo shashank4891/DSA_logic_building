c75afc9e3b6c4bef05d40723eab39f1ec54e01f5
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-02 22:24:40 +0530

    Update Python_pattern4.py

--- Star_Pattern/Pattern4/Python_pattern4.py
+++ Star_Pattern/Pattern4/Python_pattern4.py
@@ -1,1 +1,10 @@
+def pattern4(N):
+    for i in range(1, N + 1):
+        row = ""
+        for j in range(1, i + 1):
+            row += str(i) + " "
+        print(row)
+
+N = int(input("Enter the number of rows: "))
+pattern4(N)
 
