290036113834f0331808de4c6cf48b83af8de7dd
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-02 22:21:56 +0530

    Update Python_pattern2.py

--- Star_Pattern/Pattern2/Python_pattern2.py
+++ Star_Pattern/Pattern2/Python_pattern2.py
@@ -1,1 +1,10 @@
+def pattern2(N):
+    for i in range(N):
+        row = ""
+        for j in range(i + 1):
+            row += "* "
+        print(row)
+
+N = int(input("Enter the number of rows: "))
+pattern2(N)
 
