ba0a8a1c4703e4800d3329c00bf9733e576264dd
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-03 22:36:31 +0530

    Update JS_pattern6.js

--- Star_Pattern/Pattern6/JS_pattern6.js
+++ Star_Pattern/Pattern6/JS_pattern6.js
@@ -1,1 +1,14 @@
+function pattern6(N) {
+    for (let i = 0; i < N; i++) {
+        let row = "";
+        for (let j = N; j > i; j--) {
+            row += (N - j + 1) + " ";
+        }
+        console.log(row);
+    }
+}
+
+const N = parseInt(prompt("Enter the number of rows:"));
+
+pattern6(N);
 
