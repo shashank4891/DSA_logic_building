4e100a594c0a8a347c410871f9c25201ebc822c3
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-03 22:37:53 +0530

    Update Python_pattern6.py

--- Star_Pattern/Pattern6/Python_pattern6.py
+++ Star_Pattern/Pattern6/Python_pattern6.py
@@ -1,1 +1,10 @@
+def pattern6(N):
+    for i in range(N):
+        row = ""
+        for j in range(N, i, -1):
+            row += str(N - j + 1) + " "
+        print(row)
+
+N = int(input("Enter the number of rows: "))
+pattern6(N)
 
