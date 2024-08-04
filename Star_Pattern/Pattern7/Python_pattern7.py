70031db49a37b835827cc3fdb81c805887f2d02d
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-04 19:59:14 +0530

    Update Python_pattern7.py

--- Star_Pattern/Pattern7/Python_pattern7.py
+++ Star_Pattern/Pattern7/Python_pattern7.py
@@ -1,1 +1,10 @@
+def pattern7(N):
+    for i in range(N):
+        row = " " * (N - i - 1)
+        row += "*" * (2 * i + 1)
+        row += " " * (N - i - 1)
+        print(row)
+
+N = int(input("Enter the number of rows:"))
+pattern7(N)
 
