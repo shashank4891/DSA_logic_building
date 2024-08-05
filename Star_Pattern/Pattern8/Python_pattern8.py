c801db7c5cd3b3058a2595ee73a2cbae010cc1ae
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-05 16:00:54 +0530

    Update Python_pattern8.py

--- Star_Pattern/Pattern8/Python_pattern8.py
+++ Star_Pattern/Pattern8/Python_pattern8.py
@@ -1,1 +1,11 @@
 
+def pattern8(N):
+    for i in range(N):
+        row = ""
+        row += " " * i
+        row += "*" * (2 * N - (2 * i + 1))
+        row += " " * i
+        print(row)
+
+N = int(input("Enter the number of rows: "))
+pattern8(N)
