374d4d7cf8f8e086e4b5baec159531628b40c139
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-06 19:59:26 +0530

    Update JS_pattern9.js

--- Star_Pattern/Pattern9/JS_pattern9.js
+++ Star_Pattern/Pattern9/JS_pattern9.js
@@ -1,1 +1,25 @@
+function erectPyramid(N) {
+    for (let i = 0; i < N; i++) {
+        let row = "";
+        row += " ".repeat(N - i - 1);
+        row += "*".repeat(2 * i + 1);
+        row += " ".repeat(N - i - 1);
+        console.log(row);
+    }
+}
+
+function invertedPyramid(N) {
+    for (let i = 0; i < N; i++) {
+        let row = "";
+        row += " ".repeat(i);
+        row += "*".repeat(2 * N - (2 * i + 1));
+        row += " ".repeat(i);
+        console.log(row);
+    }
+}
+
+const N = parseInt(prompt("Enter the number of rows:"));
+
+erectPyramid(N);
+invertedPyramid(N);
 
