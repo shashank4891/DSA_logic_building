8668983c50b35a84300dc80a8a9e5d693c78a0bd
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-06 20:01:40 +0530

    Update Python_pattern9.py

--- Star_Pattern/Pattern9/Python_pattern9.py
+++ Star_Pattern/Pattern9/Python_pattern9.py
@@ -1,1 +1,19 @@
+def erect_pyramid(N):
+    for i in range(N):
+        row = " " * (N - i - 1)
+        row += "*" * (2 * i + 1)
+        row += " " * (N - i - 1)
+        print(row)
+
+def inverted_pyramid(N):
+    for i in range(N):
+        row = " " * i
+        row += "*" * (2 * N - (2 * i + 1))
+        row += " " * i
+        print(row)
+
+N = int(input("Enter the number of rows: "))
+
+erect_pyramid(N)
+inverted_pyramid(N)
 
