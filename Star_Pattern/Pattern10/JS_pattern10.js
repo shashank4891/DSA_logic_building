b7a27b741c2dfc0c837afd416b1e73ee15e9bddc
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-07 23:53:00 +0530

    Update JS_pattern10.js

--- Star_Pattern/Pattern10/JS_pattern10.js
+++ Star_Pattern/Pattern10/JS_pattern10.js
@@ -1,1 +1,18 @@
+function pattern10(N) {
+    for (let i = 1; i <= 2 * N - 1; i++) {
+        let stars = i;
+        if (i > N) stars = 2 * N - i;
+
+        let row = "";
+        for (let j = 1; j <= stars; j++) {
+            row += "*";
+        }
+        
+        console.log(row);
+    }
+}
+
+const N = parseInt(prompt("Enter the number of rows:"));
+
+pattern10(N);
 
