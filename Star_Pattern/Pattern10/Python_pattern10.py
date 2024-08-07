b164d07efb1a87879f6f316a5570747d3ed1d938
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-07 23:53:56 +0530

    Update Python_pattern10.py

--- Star_Pattern/Pattern10/Python_pattern10.py
+++ Star_Pattern/Pattern10/Python_pattern10.py
@@ -1,1 +1,12 @@
+def pattern10(N):
+    for i in range(1, 2 * N):
+        stars = i
+        if i > N:
+            stars = 2 * N - i
+
+        row = "*" * stars
+        print(row)
+
+N = int(input("Enter the number of rows: "))
+pattern10(N)
 
