77288ad8f8f75edf53a17de5c0038ea668a2d6e4
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-09 23:27:23 +0530

    Update JS_pattern11.js

--- Star_Pattern/Pattern11/JS_pattern11.js
+++ Star_Pattern/Pattern11/JS_pattern11.js
@@ -1,1 +1,17 @@
+function pattern11(N) {
+    for (let i = 0; i < N; i++) {
+        let start = i % 2 === 0 ? 1 : 0;
+        let row = "";
+        
+        for (let j = 0; j <= i; j++) {
+            row += start;
+            start = 1 - start;
+        }
+        
+        console.log(row);
+    }
+}
+
+const N = parseInt(prompt("Enter the number of rows:"));
+pattern11(N);
 
