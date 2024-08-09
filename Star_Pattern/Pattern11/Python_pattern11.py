fddcb2cef306dc89dae25626aadd4d440edd91e8
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-09 23:28:06 +0530

    Update Python_pattern11.py

--- Star_Pattern/Pattern11/Python_pattern11.py
+++ Star_Pattern/Pattern11/Python_pattern11.py
@@ -1,1 +1,12 @@
+def pattern11(N):
+    for i in range(N):
+        start = 1 if i % 2 == 0 else 0
+        row = ""
+        for j in range(i + 1):
+            row += str(start)
+            start = 1 - start
+        print(row)
+
+N = int(input("Enter the number of rows: "))
+pattern11(N)
 
