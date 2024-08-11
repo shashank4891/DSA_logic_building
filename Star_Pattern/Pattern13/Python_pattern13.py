4dd8bf181624d37a297d24f8f0e57765cd4252fa
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-11 13:19:57 +0530

    Update Python_pattern13.py

--- Star_Pattern/Pattern13/Python_pattern13.py
+++ Star_Pattern/Pattern13/Python_pattern13.py
@@ -1,1 +1,16 @@
+def pattern13(N):
+    num = 1
+    
+    for i in range(1, N + 1):
+        row = ""
+        
+        for j in range(1, i + 1):
+            row += str(num) + " "
+            num += 1
+        
+        print(row)
+
+# Take user input
+N = int(input("Enter the number of rows:"))
+pattern13(N)
 
