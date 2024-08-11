651b50fa51e41e0989e58d6610204b6b83bd9f4d
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-11 13:18:18 +0530

    Update JS_pattern13.js

--- Star_Pattern/Pattern13/JS_pattern13.js
+++ Star_Pattern/Pattern13/JS_pattern13.js
@@ -1,1 +1,18 @@
+function pattern13(N) {
+    let num = 1;
+    
+    for (let i = 1; i <= N; i++) {
+        let row = "";
+        
+        for (let j = 1; j <= i; j++) {
+            row += num + " ";
+            num += 1;
+        }
+        
+        console.log(row);
+    }
+}
+
+const N = parseInt(prompt("Enter the number of rows:"));
+pattern13(N);
 
