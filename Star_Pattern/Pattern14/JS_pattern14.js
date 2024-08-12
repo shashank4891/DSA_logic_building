d0b7077da0c993980a3348177e23fca01e06d764
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-12 21:25:08 +0530

    Update JS_pattern14.js

--- Star_Pattern/Pattern14/JS_pattern14.js
+++ Star_Pattern/Pattern14/JS_pattern14.js
@@ -1,1 +1,14 @@
+function pattern14(N) {
+    for (let i = 0; i < N; i++) {
+        let row = "";
+        for (let ch = 'A'; ch <= String.fromCharCode('A'.charCodeAt(0) + i); ch = String.fromCharCode(ch.charCodeAt(0) + 1)) {
+            row += ch + " ";
+        }
+        console.log(row);
+    }
+}
+
+const N = parseInt(prompt("Enter the number of rows:"));
+
+pattern14(N);
 
