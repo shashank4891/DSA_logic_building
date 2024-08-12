4bbf6a9b9b7d84ff2b75b843b75b89cc7b029540
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-12 21:26:31 +0530

    Update Python_pattern14.py

--- Star_Pattern/Pattern14/Python_pattern14.py
+++ Star_Pattern/Pattern14/Python_pattern14.py
@@ -1,1 +1,11 @@
+def pattern14(N):
+    for i in range(N):
+        row = ""
+        for ch in range(ord('A'), ord('A') + i + 1):
+            row += chr(ch) + " "
+        print(row)
+
+N = int(input("Enter the number of rows:"))
+
+pattern14(N)
 
