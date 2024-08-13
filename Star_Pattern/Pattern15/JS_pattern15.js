39f502ad34bd5717c1b2e1c729105f3d053b9263
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-13 21:09:27 +0530

    Update JS_pattern15.js

--- Star_Pattern/Pattern15/JS_pattern15.js
+++ Star_Pattern/Pattern15/JS_pattern15.js
@@ -1,1 +1,14 @@
+function pattern15(N) {
+    for (let i = 0; i < N; i++) {
+        let row = "";
+        for (let ch = 'A'; ch <= String.fromCharCode('A'.charCodeAt(0) + (N - i - 1)); ch = String.fromCharCode(ch.charCodeAt(0) + 1)) {
+            row += ch + " ";
+        }
+        console.log(row);
+    }
+}
+
+const N = parseInt(prompt("Enter the number of rows:"));
+
+pattern15(N);
 
