6aed4112bb35134b86be30aa79f2172b0b0b15fe
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-13 21:10:24 +0530

    Update Python_pattern15.py

--- Star_Pattern/Pattern15/Python_pattern15.py
+++ Star_Pattern/Pattern15/Python_pattern15.py
@@ -1,1 +1,11 @@
+def pattern15(N):
+    for i in range(N):
+        row = ""
+        for ch in range(ord('A'), ord('A') + (N - i)):
+            row += chr(ch) + " "
+        print(row)
+
+N = int(input("Enter the number of rows:"))
+
+pattern15(N)
 
