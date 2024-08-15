9e048ab8d62b1f1986ea2dcd2c1fa49c79928525
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-15 21:51:04 +0530

    Update Python_pattern16.py

--- Star_Pattern/Pattern16/Python_pattern16.py
+++ Star_Pattern/Pattern16/Python_pattern16.py
@@ -1,1 +1,11 @@
+def pattern16(N):
+    for i in range(N):
+        row = ""
+        for j in range(i + 1):
+            row += chr(ord('A') + i) + " "
+        print(row.strip())
+
+N = int(input("Enter the number of rows: "))
+
+pattern16(N)
 
