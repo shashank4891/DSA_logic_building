c4b4d2a6b295120b7de4ad9c3c377060107ab163
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-15 21:50:39 +0530

    Update JS_pattern16.js

--- Star_Pattern/Pattern16/JS_pattern16.js
+++ Star_Pattern/Pattern16/JS_pattern16.js
@@ -1,1 +1,14 @@
+function pattern16(N) {
+    for (let i = 0; i < N; i++) {
+        let row = "";
+        for (let j = 0; j <= i; j++) {
+            row += String.fromCharCode('A'.charCodeAt(0) + i) + " ";
+        }
+        console.log(row);
+    }
+}
+
+const N = parseInt(prompt("Enter the number of rows:"));
+
+pattern16(N);
 
