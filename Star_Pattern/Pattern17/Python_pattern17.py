08477f070f2d3eb11d535e2c26afe7403a1f956a
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-16 19:02:52 +0530

    Update Python_pattern17.py

--- Star_Pattern/Pattern17/Python_pattern17.py
+++ Star_Pattern/Pattern17/Python_pattern17.py
@@ -1,1 +1,25 @@
+def pattern17(N):
+    for i in range(N):
+        # Print spaces before characters
+        row = " " * (N - i - 1)
+        
+        # Print characters
+        ch = ord('A')
+        breakpoint = (2 * i + 1) // 2
+        for j in range(1, 2 * i + 2):
+            row += chr(ch)
+            if j <= breakpoint:
+                ch += 1
+            else:
+                ch -= 1
+        
+        # Print spaces after characters
+        row += " " * (N - i - 1)
+        
+        # Print the row
+        print(row)
+
+# Take input from the user
+N = int(input("Enter the value of N: "))
+pattern17(N)
 
