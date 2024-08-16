f0caca87f3475cfc48dd082459e5ebf548907712
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-16 19:01:53 +0530

    Update JS_pattern17.js

--- Star_Pattern/Pattern17/JS_pattern17.js
+++ Star_Pattern/Pattern17/JS_pattern17.js
@@ -1,1 +1,32 @@
+function pattern17(N) {
+    for (let i = 0; i < N; i++) {
+        let row = "";
+        
+        // Print spaces before characters
+        for (let j = 0; j < N - i - 1; j++) {
+            row += " ";
+        }
+        
+        // Print characters
+        let ch = 'A'.charCodeAt(0);
+        let breakpoint = Math.floor((2 * i + 1) / 2);
+        for (let j = 1; j <= 2 * i + 1; j++) {
+            row += String.fromCharCode(ch);
+            if (j <= breakpoint) ch++;
+            else ch--;
+        }
+        
+        // Print spaces after characters
+        for (let j = 0; j < N - i - 1; j++) {
+            row += " ";
+        }
+        
+        // Print the row
+        console.log(row);
+    }
+}
+
+// Take input from the user
+let N = parseInt(prompt("Enter the value of N: "));
+pattern17(N);
 
