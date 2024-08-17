48c3dca2c842f6ac2b4befe9429c0e658267453e
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-17 23:36:19 +0530

    Update Python_pattern18.py

--- Star_Pattern/Pattern18/Python_pattern18.py
+++ Star_Pattern/Pattern18/Python_pattern18.py
@@ -1,1 +1,15 @@
+def pattern18(N):
+    for i in range(N):
+        row = ""
+        
+        # Inner loop for printing the alphabets
+        for ch in range(ord('A') + N - 1 - i, ord('A') + N):
+            row += chr(ch) + " "
+        
+        # Print the row
+        print(row)
+
+# Take input from the user
+N = int(input("Enter the value of N: "))
+pattern18(N)
 
