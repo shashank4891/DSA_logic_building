4884bc612cfca1c1f876fd28680b82f994aeb6ca
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2024-08-18 23:41:03 +0530

    Update Python_pattern19.py

--- Star_Pattern/Pattern19/Python_pattern19.py
+++ Star_Pattern/Pattern19/Python_pattern19.py
@@ -1,1 +1,50 @@
+def pattern19(N):
+    iniS = 0
+    
+    # Upper half of the pattern
+    for i in range(N):
+        row = ""
+        
+        # Print stars
+        for j in range(N - i):
+            row += "*"
+        
+        # Print spaces
+        for j in range(iniS):
+            row += " "
+        
+        # Print stars
+        for j in range(N - i):
+            row += "*"
+        
+        print(row)
+        
+        # Increase spaces by 2 for the next row
+        iniS += 2
+    
+    # Lower half of the pattern
+    iniS = 2 * N - 2
+    for i in range(1, N + 1):
+        row = ""
+        
+        # Print stars
+        for j in range(i):
+            row += "*"
+        
+        # Print spaces
+        for j in range(iniS):
+            row += " "
+        
+        # Print stars
+        for j in range(i):
+            row += "*"
+        
+        print(row)
+        
+        # Decrease spaces by 2 for the next row
+        iniS -= 2
+
+# Take input from the user
+N = int(input("Enter the value of N: "))
+pattern19(N)
 
